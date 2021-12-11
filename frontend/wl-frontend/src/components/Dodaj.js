5f04239a7309b02b3d7e758d98130a55aa8d8ecf
Nik Kovačević <nik.kovacevic@student.um.si>
2021-12-11 20:53:33 +0100

    Beautify-an frontend

--- frontend/wl-frontend/src/components/Dodaj.js
+++ frontend/wl-frontend/src/components/Dodaj.js
@@ -10,7 +10,7 @@ export default function Dodaj() {
     const onSubmitForm = async e => {
         e.preventDefault();
         try {
-            
+
             const body = {
                 'naslov': naslov,
                 'opis': opis,
@@ -18,14 +18,14 @@ export default function Dodaj() {
             }
             const response = await fetch("http://localhost:8080/savetodos", {
                 method: "POST",
-                headers: {"Content-Type": "application/json" },
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(body)
             });
-            
+
             window.location = "/";
 
         } catch (error) {
-            console.error(error.message)
+            console.error(error.message);
         }
 
         setNaslov("");
@@ -35,19 +35,17 @@ export default function Dodaj() {
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="bggradient">
-                
                 <div className="formWrapper">
                     <form className="todoform" onSubmit={onSubmitForm}>
 
-                        <label htmlFor="naslovinput">Naslov:</label><br/>
-                        <input type="text" placeholder="Naslov" value={naslov} name="naslovinput" className="naslovinput" onChange={e => setNaslov(e.target.value)} required/><br/>
-                        <label htmlFor="opisinput">Opis:</label><br/>
-                        <input type="text" placeholder="Opis" value={opis} name="opisinput" className="opisinput" onChange={e => setOpis(e.target.value)}/><br/>
-                        <label htmlFor="nujnostinput">Nujnost 1-3 (1 najmanj, 3 najbolj):</label><br/>
-                        <input type="number" min="1" max="3" placeholder="Nujnost" value={nujnost} name="nujnostinput" className="nujnostinput" onChange={e => setNujnost(e.target.value)}/><br/>
-
+                        <label htmlFor="naslovinput">Naslov:</label><br />
+                        <input type="text" placeholder="Naslov" value={naslov} name="naslovinput" className="naslovinput" onChange={e => setNaslov(e.target.value)} required /><br />
+                        <label htmlFor="opisinput">Opis:</label><br />
+                        <input type="text" placeholder="Opis" value={opis} name="opisinput" className="opisinput" onChange={e => setOpis(e.target.value)} /><br />
+                        <label htmlFor="nujnostinput">Nujnost 1-3 (1 najmanj, 3 najbolj):</label><br />
+                        <input type="number" min="1" max="3" placeholder="Nujnost" value={nujnost} name="nujnostinput" className="nujnostinput" onChange={e => setNujnost(e.target.value)} /><br />
                         <button className="buttondodaj">Dodaj</button>
 
                     </form>
